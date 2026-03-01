264ada65f9b59f609c6155f6a8febe7ada92fa35
danushr <101976905+danushr@users.noreply.github.com>
2026-03-01 15:57:40 -0500

    Update reading.js

--- reading.js
+++ reading.js
@@ -1,22 +1,87 @@
 const dailyReading = {
     date: "Sunday, March 1, 2026",
-    title: "First Sunday of Lent",
+    title: "Second Sunday of Lent",
     reading: {
         "First Reading": {
-            citation: "Genesis 9:8-15",
-            body: "God said to Noah and to his sons with him: 'See, I am now establishing my covenant with you and your descendants after you...'"
+            citation: "Genesis 12:1-4a",
+            body: "The LORD said to Abram:
+“Go forth from the land of your kinsfolk
+and from your father’s house to a land that I will show you.
+
+“I will make of you a great nation,
+and I will bless you;
+I will make your name great,
+so that you will be a blessing.
+I will bless those who bless you
+and curse those who curse you.
+All the communities of the earth
+shall find blessing in you.”
+
+Abram went as the LORD directed him.'"
         },
         "Responsorial Psalm": {
             citation: "Psalm 25:4-5, 6-7, 8-9",
-            body: "Your ways, O LORD, make known to me; teach me your paths..."
+            body: "R. (22) Lord, let your mercy be on us, as we place our trust in you.
+Upright is the word of the LORD,
+and all his works are trustworthy.
+He loves justice and right;
+of the kindness of the LORD the earth is full.
+R. Lord, let your mercy be on us, as we place our trust in you.
+See, the eyes of the LORD are upon those who fear him,
+upon those who hope for his kindness,
+To deliver them from death
+and preserve them in spite of famine.
+R. Lord, let your mercy be on us, as we place our trust in you.
+Our soul waits for the LORD,
+who is our help and our shield.
+May your kindness, O LORD, be upon us
+who have put our hope in you.
+R. Lord, let your mercy be on us, as we place our trust in you."
         },
         "Second Reading": {
             citation: "1 Peter 3:18-22",
-            body: "Beloved: Christ suffered for sins once, the righteous for the sake of the unrighteous..."
+            body: "Beloved:
+Bear your share of hardship for the gospel
+with the strength that comes from God.
+
+He saved us and called us to a holy life,
+not according to our works
+but according to his own design
+and the grace bestowed on us in Christ Jesus before time began,
+but now made manifest
+through the appearance of our savior Christ Jesus,
+who destroyed death and brought life and immortality
+to light through the gospel."
         },
         "Gospel": {
             citation: "Mark 1:12-15",
-            body: "The Spirit drove Jesus out into the desert, and he remained in the desert for forty days, tempted by Satan..."
+            body: "Jesus took Peter, James, and John his brother,
+and led them up a high mountain by themselves.
+And he was transfigured before them;
+his face shone like the sun
+and his clothes became white as light.
+And behold, Moses and Elijah appeared to them,
+conversing with him.
+Then Peter said to Jesus in reply,
+“Lord, it is good that we are here.
+If you wish, I will make three tents here,
+one for you, one for Moses, and one for Elijah.”
+While he was still speaking, behold,
+a bright cloud cast a shadow over them,
+then from the cloud came a voice that said,
+“This is my beloved Son, with whom I am well pleased;
+listen to him.”
+When the disciples heard this, they fell prostrate
+and were very much afraid.
+But Jesus came and touched them, saying,
+“Rise, and do not be afraid.”
+And when the disciples raised their eyes,
+they saw no one else but Jesus alone.
+
+As they were coming down from the mountain,
+Jesus charged them,
+“Do not tell the vision to anyone
+until the Son of Man has been raised from the dead.”"
         }
     }
 };

824e563860bbcecee7307b833b22b38afa2351f9
danushr <101976905+danushr@users.noreply.github.com>
2026-03-01 15:48:10 -0500

    Create reading.js

---
+++ reading.js
@@ -0,0 +1,10 @@
+const dailyReadings = {
+    date: "Sunday, March 1, 2026",
+    title: "First Sunday of Lent",
+    reading: {
+        "First Reading": "Genesis 9:8-15...",
+        "Responsorial Psalm": "Psalm 25:4-5, 6-7...",
+        "Second Reading": "1 Peter 3:18-22...",
+        "Gospel": "Mark 1:12-15..."
+    }
+};
